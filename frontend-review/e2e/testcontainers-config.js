573cac53b3c288b04b25deb7ee028be305129457
fernando <fernandoadt1@gmail.com>
2026-07-07 21:13:57 +0200

    trying new E2E test flow

--- frontend-review/e2e/testcontainers-config.js
+++ frontend-review/e2e/testcontainers-config.js
@@ -1,22 +1,8 @@
 const path = require('path');
-const { execSync } = require('child_process');
 const { DockerComposeEnvironment, Wait } = require('testcontainers');
 
-const composeFile = path.resolve(__dirname, '../docker-compose-frontend-development.yml');
-const terraformDir = path.resolve(__dirname, '../../terraform');
+const composeFile = path.resolve(__dirname, '../docker-compose-e2e-apps.yml');
 let testContainersRuntime;
-
-function provisionAwsResourcesWithTerraform() {
-  execSync('terraform init', {
-    cwd: terraformDir,
-    stdio: 'inherit',
-  });
-
-  execSync('terraform apply -var-file=local.tfvars -auto-approve', {
-    cwd: terraformDir,
-    stdio: 'inherit',
-  });
-}
 
 async function setupContainers() {
 
@@ -24,14 +10,11 @@ async function setupContainers() {
       path.dirname(composeFile),
       path.basename(composeFile)
     )
-      .withWaitStrategy('localstack', Wait.forLogMessage('Ready'))
       .withWaitStrategy('review-collector', Wait.forHttp("/actuator/health", 8080).forStatusCode(200))
       .withWaitStrategy('review-analyzer', Wait.forHttp("/actuator/health", 8081).forStatusCode(200))
       .up();
 
   console.log('Containers started successfully.');
-
-  provisionAwsResourcesWithTerraform();
 
   return testContainersRuntime;
 }
