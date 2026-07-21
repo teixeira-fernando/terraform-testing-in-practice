555eb6f1d295854b6e8c39b478dddcf238c738b8
fernando <fernandoadt1@gmail.com>
2026-07-21 16:08:35 +0200

    deleted review-analyzer and review-collector leftovers

--- review-analyzer/src/main/java/com/teixeirafernando/review/analyzer/ApplicationProperties.java
+++
@@ -1,6 +0,0 @@
-package com.teixeirafernando.review.analyzer;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "app")
-public record ApplicationProperties(String queue, String bucket) {}
